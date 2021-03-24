a14c29b219850c4666efe1d524970a99baba6e0b
askharley <harl3y.fer@gmail.com>
2021-03-24 10:42:40 +0200

    Added React application folder.

---
+++ hello-docker/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello Docker!</h1>
+      <img src="https://miro.medium.com/max/336/0*iXuwZtbBvsYPPkrY.png" />
+    </div>
+  );
+}
